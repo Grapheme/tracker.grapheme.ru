90a14a9557fe5aaf9b3fb9815dab60b1da28b27f
Владимир <vkharseev@gmail.com>
2015-02-05 12:27:54 +0300

    Задача теперь только одна активная для текущего пользователя

--- app/controllers/projects-administrator/TimeSheetsController.php
+++ app/controllers/projects-administrator/TimeSheetsController.php
@@ -45,7 +45,14 @@ class TimeSheetsController extends \BaseController {
 		$validator = Validator::make(Input::all(),ProjectTask::$rules);
 		$set_date = Input::get('set_date') ? Input::get('set_date') : date('Y-m-d');
 		if($validator->passes()):
-			if($task = ProjectTask::create(['project_id' => Input::get('project'), 'user_id' => Input::get('performer'),'note' => Input::get('note'),'start_status' => 1,'start_date' => date('Y-m-d H:i:s'),'set_date'=>$set_date,'lead_time'=>str2secLeadTime(Input::get('lead_time'))])):
+			if (ProjectTask::where('user_id',Auth::user()->id)->where('start_status',1)->where('stop_status',0)->exists()):
+				$start_status = 0;
+				$start_date = '0000-00-00 00:00:00';
+			else:
+				$start_status = 1;
+				$start_date = date('Y-m-d H:i:s');
+			endif;
+			if($task = ProjectTask::create(['project_id' => Input::get('project'), 'user_id' => Input::get('performer'),'note' => Input::get('note'),'start_status' => $start_status,'start_date' => $start_date,'set_date'=>$set_date,'lead_time'=>str2secLeadTime(Input::get('lead_time'))])):
 				return Redirect::route('project_admin.timesheets.index',['date'=>$set_date])->with('message','Задача создана успешно.');
 			else:
 				return Redirect::back()->with('message','Возникла ошибка при записи в БД');
@@ -95,7 +102,7 @@ class TimeSheetsController extends \BaseController {
 		if ($task = ProjectTask::where('id',$id)->first()):
 			if(Project::where('id',$task->project_id)->where('superior_id',Auth::user()->id)->first()):
 				$task->delete();
-				return Redirect::route('project_admin.timesheets.index')->with('message','Задача удалена успешно.');
+				return Redirect::back()->with('message','Задача удалена успешно.');
 			endif;
 		endif;
 		App::abort(404);
@@ -112,16 +119,14 @@ class TimeSheetsController extends \BaseController {
 					if (Input::get('run') == 0):
 						$task->stop_status = 1;
 						$task->stop_date = date('Y-m-d H:i:s');
-					elseif(Input::get('run') == 1 && $task->start_status == 0):
+					elseif(Input::get('run') == 1):
+						$dt = myDateTime::getDiffTimeStamp($task->stop_date,$task->start_date);
+						$task->lead_time += $dt > 60 ? $dt : 0;
 						$task->start_status = 1;
 						$task->start_date = date('Y-m-d H:i:s');
 						$task->stop_status = 0;
 						$task->stop_date = '0000-00-00 00:00:00';
-					elseif(Input::get('run') == 1 && $task->start_status == 1):
-						$task->lead_time += myDateTime::getDiffTimeStamp($task->stop_date,$task->start_date);
-						$task->start_date = date('Y-m-d H:i:s');
-						$task->stop_status = 0;
-						$task->stop_date = '0000-00-00 00:00:00';
+						ProjectTask::where('user_id',Auth::user()->id)->where('stop_status',0)->where('id','!=',$task->id)->update(['stop_status'=>1,'stop_date'=>date('Y-m-d H:i:s'),'updated_at'=>date('Y-m-d H:i:s')]);
 					endif;
 					$task->save();
 					$task->touch();
