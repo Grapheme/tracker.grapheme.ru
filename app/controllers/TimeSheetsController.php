8c346f2cde04f13d6d09e483b4b4c90474f5c8f2
Владимир <vkharseev@gmail.com>
2015-02-05 17:42:59 +0300

    fix переключения задач для админа проектов

--- app/controllers/TimeSheetsController.php
+++ app/controllers/TimeSheetsController.php
@@ -45,13 +45,19 @@ class TimeSheetsController extends \BaseController {
 		$validator = Validator::make(Input::all(),ProjectTask::$rules);
 		$set_date = Input::get('set_date') ? Input::get('set_date') : date('Y-m-d');
 		if($validator->passes()):
-			if (ProjectTask::where('user_id',Auth::user()->id)->where('start_status',1)->where('stop_status',0)->exists()):
+			if (Input::get('performer') == Auth::user()->id):
+				if (ProjectTask::where('user_id',Auth::user()->id)->where('start_status',1)->where('stop_status',0)->exists()):
+					$start_status = 0;
+					$start_date = '0000-00-00 00:00:00';
+				else:
+					$start_status = 1;
+					$start_date = date('Y-m-d H:i:s');
+				endif;
+			else:
 				$start_status = 0;
 				$start_date = '0000-00-00 00:00:00';
-			else:
-				$start_status = 1;
-				$start_date = date('Y-m-d H:i:s');
 			endif;
+
 			if($task = ProjectTask::create(['project_id' => Input::get('project'), 'user_id' => Input::get('performer'),'note' => Input::get('note'),'start_status' => $start_status,'start_date' => $start_date,'set_date'=>$set_date,'lead_time'=>str2secLeadTime(Input::get('lead_time'))])):
 				return Redirect::route('timesheets.index',['date'=>$set_date])->with('message','Задача создана успешно.');
 			else:
@@ -119,36 +125,7 @@ class TimeSheetsController extends \BaseController {
 						$task->start_date = date('Y-m-d H:i:s');
 						$task->stop_status = 0;
 						$task->stop_date = '0000-00-00 00:00:00';
-						ProjectTask::where('user_id',Auth::user()->id)->where('stop_status',0)->where('id','!=',$task->id)->update(['stop_status'=>1,'stop_date'=>date('Y-m-d H:i:s'),'updated_at'=>date('Y-m-d H:i:s')]);
-					endif;
-					$task->save();
-					$task->touch();
-				endif;
-				return Redirect::back();
-			endif;
-		endif;
-		App::abort(404);
-	}
-
-	public function PerformerRunningTimer(){
-
-		$validator = Validator::make(Input::all(),['task'=>'integer|required','run'=>'integer|required']);
-		if($validator->passes()):
-			if ($task = ProjectTask::where('id',Input::get('task'))->first()):
-				$teamIDs = Team::where('superior_id',Auth::user()->id)->lists('cooperator_id');
-				$teamIDs[] = Auth::user()->id;
-				if (in_array($task->user_id,$teamIDs)):
-					if (Input::get('run') == 0):
-						$task->stop_status = 1;
-						$task->stop_date = date('Y-m-d H:i:s');
-					elseif(Input::get('run') == 1):
-						$dt = myDateTime::getDiffTimeStamp($task->stop_date,$task->start_date);
-						$task->lead_time += $dt > 60 ? $dt : 0;
-						$task->start_status = 1;
-						$task->start_date = date('Y-m-d H:i:s');
-						$task->stop_status = 0;
-						$task->stop_date = '0000-00-00 00:00:00';
-						ProjectTask::where('user_id',Auth::user()->id)->where('stop_status',0)->where('id','!=',$task->id)->update(['stop_status'=>1,'stop_date'=>date('Y-m-d H:i:s'),'updated_at'=>date('Y-m-d H:i:s')]);
+						ProjectTask::where('user_id',$task->user_id)->where('stop_status',0)->where('id','!=',$task->id)->update(['stop_status'=>1,'stop_date'=>date('Y-m-d H:i:s'),'updated_at'=>date('Y-m-d H:i:s')]);
 					endif;
 					$task->save();
 					$task->touch();
