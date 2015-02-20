870f98170205cb66a226b885eb0d926516924905
Владимир <vkharseev@gmail.com>
2015-02-20 15:09:05 +0300

    Импорт с бейскемпа проектов

--- app/database/migrations/2015_02_11_125751_create_basecamp_table.php
+++ app/database/migrations/2015_02_11_125751_create_basecamp_table.php
@@ -8,8 +8,8 @@ class CreateBasecampTable extends Migration {
 	public function up(){
 		Schema::create('basecamp_accounts', function(Blueprint $table){
 			$table->increments('id');
-			$table->integer('basecamp_identity_id')->nullable()->default(0)->unsigned();
-			$table->integer('basecamp_user_id')->nullable()->default(0)->unsigned();
+            $table->integer('user_id')->nullable()->default(0)->unsigned();
+            $table->integer('basecamp_identity_id')->nullable()->default(0)->unsigned();
 			$table->text('token')->nullable();
 			$table->timestamp('token_expires_at')->default('0000-00-00 00:00:00');
 			$table->text('basecamp_accounts_list')->nullable();
