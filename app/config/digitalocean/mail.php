51fdce19eaa49c46dd321e23e421e86c473e30ab
Владимир <vkharseev@gmail.com>
2015-02-27 18:06:29 +0300

    mail

---
+++ app/config/digitalocean/mail.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+	'feedback' => array(
+		'address' => 'support@grapheme.ru',
+	),
+
+	'driver' => 'smtp',
+	'host' => 'smtp.yandex.ru',
+	'port' => 465,
+	'from' => array('address' => 'dah-sl@yandex.ru', 'name' => 'Tracker Grapheme'),
+	'encryption' => 'ssl',
+	'username' => 'dah-sl@yandex.ru',
+	'password' => '534msd8HHS',
+	'sendmail' => '/usr/sbin/sendmail -bs',
+	'pretend' => FALSE,
+);
