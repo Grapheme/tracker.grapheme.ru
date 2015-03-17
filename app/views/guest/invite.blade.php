9f69399b76db4f83755ba35d07cc059397dff543
Владимир <vkharseev@gmail.com>
2015-03-17 13:17:56 +0300

    Отчеты

--- app/views/guest/invite.blade.php
+++ app/views/guest/invite.blade.php
@@ -3,7 +3,7 @@
 @section('register')
 @section('content')
     <div class="alert alert-success" role="alert">
-        {{ $invitee->fio }} приветствует Вас в его комманде! И цемкает в попку....
+        {{ $invitee->fio }} приветствует Вас в его комманде!
     </div>
     <a href="{{ URL::route('dashboard') }}" class="btn btn-success">Личный кабинет</a>
 @stop
