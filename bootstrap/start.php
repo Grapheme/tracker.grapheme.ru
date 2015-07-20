6457219057e805542269f74034784732c578dc9b
Владимир <vkharseev@gmail.com>
2015-07-20 11:50:38 +0300

    timezone

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -8,4 +8,5 @@ $env = $app->detectEnvironment(array(
 $app->bindInstallPaths(require __DIR__.'/paths.php');
 $framework = $app['path.base'].'/vendor/laravel/framework/src';
 require $framework.'/Illuminate/Foundation/start.php';
+date_default_timezone_set(Config::get('app.timezone'));
 return $app;
