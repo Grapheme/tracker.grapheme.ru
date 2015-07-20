45f9a817d8321dc62b6bfc99ee454236cc773317
Владимир <vkharseev@gmail.com>
2015-07-20 11:53:33 +0300

    timezone

--- app/views/user/assets/header.blade.php
+++ app/views/user/assets/header.blade.php
@@ -5,6 +5,7 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
+                <li><a href="javascript:void(0);">{{ date("d.m.Y H:i") }}</a></li>
                 <li><a href="{{ URL::route('dashboard') }}">Dashboard</a></li>
                 <li><a href="{{ URL::route('settings') }}">Настройка</a></li>
                 <li><a href="{{ URL::route('profile') }}">Профиль</a></li>
