7fe3bfc01ec0ed5221ed8daa8076d237b9ddb2ee
Владимир <vkharseev@gmail.com>
2015-09-02 11:38:40 +0300

    выпадающие списки для статистики

--- app/views/user/reports/create.blade.php
+++ app/views/user/reports/create.blade.php
@@ -1,6 +1,7 @@
 @extends(Helper::acclayout())
 @section('style')
-{{ HTML::style(Config::get('site.theme_path').'/css/datapicker/jquery-ui-datapicker.css') }}
+    {{ HTML::style(Config::get('site.theme_path').'/css/datapicker/jquery-ui-datapicker.css') }}
+    {{ HTML::style(Config::get('site.theme_path').'/css/select2.min.css') }}
 @stop
 @section('content')
     <div class="container marketing">
@@ -12,48 +13,51 @@
     <div class="container marketing">
         <div class="row">
             <h2 class="sub-header">Список задач</h2>
+
             <div class="pull-left">
-        @if(count($tasks))
-                {{ Form::open(['route'=>['report.save','default','pdf','D'],'method'=>'post']) }}
+                @if(count($tasks))
+                    {{ Form::open(['route'=>['report.save','default','pdf','D'],'method'=>'post']) }}
                     {{ Form::hidden('begin_date',Input::has('begin_date') ? Input::get('begin_date') : (new myDateTime())->setDateString($startOfDay)->format('Y-m-d')) }}
                     {{ Form::hidden('end_date',Input::has('end_date') ? Input::get('end_date') : (new myDateTime())->setDateString($endOfDay)->format('Y-m-d')) }}
                     {{ Form::hidden('client',Input::get('client')) }}
                     {{ Form::hidden('project',Input::get('project')) }}
                     {{ Form::hidden('user',Input::get('user')) }}
                     {{ Form::submit('Экспорт в PDF',['class'=>'btn btn-success']) }}
-                {{ Form::close() }}
+                    {{ Form::close() }}
             </div>
             <div class="pull-right">
-            @if(count($clients) > 1)
-                @if(Input::has('client') && Input::get('client') > 0)
-                {{ Form::open(['route'=>['report.save','invoice','pdf','F'],'method'=>'post']) }}
-                    {{ Form::hidden('begin_date',Input::has('begin_date') ? Input::get('begin_date') : (new myDateTime())->setDateString($startOfDay)->format('Y-m-d')) }}
-                    {{ Form::hidden('end_date',Input::has('end_date') ? Input::get('end_date') : (new myDateTime())->setDateString($endOfDay)->format('Y-m-d')) }}
-                    {{ Form::hidden('client',Input::get('client')) }}
-                    {{ Form::hidden('project',Input::get('project')) }}
-                    {{ Form::hidden('user',Input::get('user')) }}
-                    {{ Form::submit('Сформирвать счет',['class'=>'btn btn-info']) }}
-                {{ Form::close() }}
-                @else
-                <p class="text-info">Для сохранения счета выберите клиента.</p>
+                @if(count($clients) > 1)
+                    @if(Input::has('client') && Input::get('client') > 0)
+                        {{ Form::open(['route'=>['report.save','invoice','pdf','F'],'method'=>'post']) }}
+                        {{ Form::hidden('begin_date',Input::has('begin_date') ? Input::get('begin_date') : (new myDateTime())->setDateString($startOfDay)->format('Y-m-d')) }}
+                        {{ Form::hidden('end_date',Input::has('end_date') ? Input::get('end_date') : (new myDateTime())->setDateString($endOfDay)->format('Y-m-d')) }}
+                        {{ Form::hidden('client',Input::get('client')) }}
+                        {{ Form::hidden('project',Input::get('project')) }}
+                        {{ Form::hidden('user',Input::get('user')) }}
+                        {{ Form::submit('Сформирвать счет',['class'=>'btn btn-info']) }}
+                        {{ Form::close() }}
+                    @else
+                        <p class="text-info">Для сохранения счета выберите клиента.</p>
+                    @endif
                 @endif
-            @endif
             </div>
             <div class="clearfix"></div>
             @include(Helper::acclayout('reports.tasks-lists'),['tasks'=>$tasks,'showTotal'=>TRUE])
-        @else
-            <p>Список пуст</p>
-        @endif
+            @else
+                <p>Список пуст</p>
+            @endif
         </div>
     </div>
 @stop
 @section('scripts')
-{{ HTML::script(Config::get('site.theme_path').'/js/datepicker/jquery.ui.datepicker.js') }}
-{{ HTML::script(Config::get('site.theme_path').'/js/datepicker/jquery.ui.datepicker-ru.js') }}
-<script type="application/javascript">
-    $("input.datepicker").datepicker({
-        minDate: "01.01.2015",
-        maxDate: "{{ date('d.m.Y') }}"
-    });
-</script>
+    {{ HTML::script(Config::get('site.theme_path').'/js/datepicker/jquery.ui.datepicker.js') }}
+    {{ HTML::script(Config::get('site.theme_path').'/js/datepicker/jquery.ui.datepicker-ru.js') }}
+    {{ HTML::script(Config::get('site.theme_path').'/js/select2.min.js') }}
+    <script type="application/javascript">
+        $("input.datepicker").datepicker({
+            minDate: "01.01.2015",
+            maxDate: "{{ date('d.m.Y') }}"
+        });
+        $("select").select2();
+    </script>
 @stop
--- app/views/user/reports/forms/report-create.blade.php
+++ app/views/user/reports/forms/report-create.blade.php
@@ -1,14 +1,14 @@
 {{ Form::open(['route'=>'report.create','method'=>'get','role'=>'form','class'=>'form-horizontal']) }}
 <div class="form-group">
     <label class="col-sm-3 control-label">От</label>
-    <div class="col-sm-3">
-        {{ Form::text('begin_date',Input::has('begin_date') ? Input::get('begin_date') : (new myDateTime())->setDateString($startOfDay)->format('d.m.Y'),['class'=>'form-control datepicker','readonly'=>'']) }}
+    <div class="col-sm-2">
+        {{ Form::text('begin_date',Input::has('begin_date') ? Input::get('begin_date') : (new myDateTime())->setDateString($startOfDay)->format('d.m.Y'),['class'=>'form-control datepicker','readonly'=>'', 'style'=>'text-align:center;']) }}
     </div>
 </div>
 <div class="form-group has-feedback">
     <label class="col-sm-3 control-label">До</label>
-    <div class="col-sm-3">
-        {{ Form::text('end_date',Input::has('end_date') ? Input::get('end_date') : (new myDateTime())->setDateString($endOfDay)->format('d.m.Y'),['class'=>'form-control datepicker', 'readonly'=>'']) }}
+    <div class="col-sm-2">
+        {{ Form::text('end_date',Input::has('end_date') ? Input::get('end_date') : (new myDateTime())->setDateString($endOfDay)->format('d.m.Y'),['class'=>'form-control datepicker', 'readonly'=>'', 'style'=>'text-align:center;']) }}
     </div>
 </div>
 @if(count($clients) > 1)
@@ -22,7 +22,7 @@
 @if(count($projects) > 1)
     <div class="form-group">
         <label class="col-sm-3 control-label">Проект</label>
-        <div class="col-sm-6">
+        <div class="col-sm-3">
             {{ Form::select('project',$projects,Input::get('project'),['class'=>'form-control','autocomplete'=>'off']) }}
         </div>
     </div>
@@ -30,7 +30,7 @@
 @if(count($users) > 1)
     <div class="form-group">
         <label class="col-sm-3 control-label">Команда</label>
-        <div class="col-sm-6">
+        <div class="col-sm-3">
             {{ Form::select('user',$users,Input::get('user'),['class'=>'form-control','autocomplete'=>'off']) }}
         </div>
     </div>
